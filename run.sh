a6e557c5143811b631d5fee937adba8d2644009e
Fabian Hinderer <uypcr@student.kit.edu>
2017-04-13 18:08:06 +0200

    finish

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#doplhin root access!
+#mount
+
 sudo -s exit
 read -rsp $'Press enter to continue...\n'
-sudo losetup -P --show /dev/loop5 storage.img
-sudo mount /dev/loop5p1 _mnt
+sudo losetup -P --show /dev/loop0 storage.img
+sudo mount -ouser,umask=0000 /dev/loop0p1 _mnt
 read -rsp $'create file!\n'
-sudo umount /dev/loop5p1
-sudo losetup -d /dev/loop5
+sudo umount /dev/loop0p1
+sudo losetup -d /dev/loop0
 cargo run
